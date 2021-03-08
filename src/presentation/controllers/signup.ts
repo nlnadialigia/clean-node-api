a2e26603663c82adb50ad2d466cf66f65ee1efb5
Nadia <nlnadialigia@gmail.com>
2021-03-07 22:16:09 -0300

    refactor: change class name of signup.ts

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,7 +3,7 @@ import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
-export class SignupController implements Controller {
+export class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
